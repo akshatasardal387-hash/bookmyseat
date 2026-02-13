859e2a9e8e1b2a4738ede1c0b42799b17d33a3e6
Akshata Sardal <akshatasardal387@gmail.com>
2026-02-13 18:47:33 +0530

    Fix Vercel handler issue

--- bookmyseat/wsgi.py
+++ bookmyseat/wsgi.py
@@ -4,15 +4,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 
 app = get_wsgi_application()
-handler = app  # <-- VERY IMPORTANT for Vercel
 
 
-# -------- AUTO CREATE SUPERUSER (for Render without shell) --------
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
+# -------- AUTO CREATE SUPERUSER (SAFE FOR VERCEL) --------
 def create_superuser():
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+
     username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
     email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
     password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
@@ -22,4 +21,4 @@ def create_superuser():
             User.objects.create_superuser(username, email, password)
 
 create_superuser()
-# ------------------------------------------------------------------
+# ---------------------------------------------------------
