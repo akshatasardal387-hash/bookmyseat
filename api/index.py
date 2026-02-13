6ab008cae16e10b9e623d866113215f1ef50e772
Akshata Sardal <akshatasardal387@gmail.com>
2026-02-13 21:32:29 +0530

    final wsgi fix for vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmyseat.settings")
 
-app = get_wsgi_application()
+application = get_wsgi_application()
