5d71f8b2c0c080f4c94a447015ed0d548a6fba47
Akshata Sardal <akshatasardal387@gmail.com>
2026-02-14 13:03:48 +0530

    updated settings

--- api/index.py
+++ api/index.py
@@ -3,7 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmyseat.settings")
 
-application = get_wsgi_application()
-
-def handler(request, context):
-    return application
+handler = get_wsgi_application()
