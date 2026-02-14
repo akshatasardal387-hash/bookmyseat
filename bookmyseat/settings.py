d04c11668410574eef9f733cbf6269cd14ceef7d
Akshata Sardal <akshatasardal387@gmail.com>
2026-02-14 13:14:21 +0530

    fixed settings final

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -67,10 +67,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-        )
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
 else:
     DATABASES = {
