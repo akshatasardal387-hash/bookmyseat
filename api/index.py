c3221f6c5abef6d98af859ca9f8437945b60e9e9
Akshata Sardal <akshatasardal387@gmail.com>
2026-02-14 12:47:09 +0530

    final handler fix

--- api/index.py
+++ api/index.py
@@ -3,4 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmyseat.settings")
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+def handler(request, context):
+    return application
